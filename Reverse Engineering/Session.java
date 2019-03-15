3d7d14fd858908567a6d066f369d2cd835a6cec2
SerbanMatei <48119059+SerbanMatei@users.noreply.github.com>
2019-03-15 09:57:16 +0200

    Add files via upload

---
+++ Reverse Engineering/Session.java
@@ -0,0 +1,15 @@
+public class Session {
+
+  private String name;
+
+  public ArrayList<Question> questionsList;
+
+  private LoggedUser owner;
+
+  public void Session( name) {
+  }
+
+  public void addQuestion( questioon) {
+  }
+
+}
